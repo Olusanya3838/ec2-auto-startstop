6a05b6e1b50d2f15f835c228059a80360f63c908
Olusanya3838 <111055604+Olusanya3838@users.noreply.github.com>
2025-05-20 14:17:22 +0100

    Update lambda_stop.py

--- lambda_stop.py
+++ lambda_stop.py
@@ -1,20 +1,25 @@
-import boto3
+import boto3
 
 def lambda_handler(event, context):
     ec2 = boto3.client('ec2')
 
     filters = [{'Name': 'tag:AutoStartStop', 'Values': ['true']}]
-    instances = ec2.describe_instances(Filters=filters)
 
-    instance_ids = [
-        instance['InstanceId']
-        for reservation in instances['Reservations']
-        for instance in reservation['Instances']
-        if instance['State']['Name'] == 'running'
-    ]
+    try:
+        response = ec2.describe_instances(Filters=filters)
+        instance_ids = []
 
-    if instance_ids:
-        print(f"Stopping instances: {instance_ids}")
-        ec2.stop_instances(InstanceIds=instance_ids)
-    else:
-        print("No running instances found.")
+        for reservation in response['Reservations']:
+            for instance in reservation['Instances']:
+                print(f"Found instance: {instance['InstanceId']} (state: {instance['State']['Name']})")
+                if instance['State']['Name'] == 'running':
+                    instance_ids.append(instance['InstanceId'])
+
+        if instance_ids:
+            print(f"Stopping instances: {instance_ids}")
+            ec2.stop_instances(InstanceIds=instance_ids)
+        else:
+            print("No running instances found with tag AutoStartStop=true")
+
+    except Exception as e:
+        print(f"Error: {str(e)}")
